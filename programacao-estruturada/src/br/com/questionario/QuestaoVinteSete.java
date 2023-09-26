d635e3b2188b7fee86af07268b837ae24e46c712
alvessh <117163985+alvessh@users.noreply.github.com>
2023-09-26 05:14:47 -0400

    adicionando questoes 27,28 e 29

---
+++ programacao-estruturada/src/br/com/questionario/QuestaoVinteSete.java
@@ -0,0 +1,32 @@
+package br.com.questionario;
+
+public class QuestaoVinteSete {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 5;
+
+		boolean igual = x == y;
+		System.out.println(igual);
+		boolean diferente = x != y;   
+		System.out.println(diferente);
+		boolean maiorQue = x > y;
+		System.out.println(maiorQue);
+		boolean menorQue = x < y;
+		System.out.println(menorQue);
+		boolean maiorOuIgual = x >= y;
+		System.out.println(maiorOuIgual);
+		boolean menorOuIgual = x <= y;
+		System.out.println(diferente);
+
+	}
+	//Saída: 
+		/*
+		 *  false
+			true
+			true
+			false
+			true
+			true
+		 */
+}
