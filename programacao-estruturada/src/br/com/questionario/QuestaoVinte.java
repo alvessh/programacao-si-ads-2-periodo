fdec1cffbeb2cf026560a6770bbb3cda0f0e17f6
alvessh <117163985+alvessh@users.noreply.github.com>
2023-10-02 20:07:30 -0400

    ajustando sintaxe

--- programacao-estruturada/src/br/com/questionario/QuestaoVinte.java
+++ programacao-estruturada/src/br/com/questionario/QuestaoVinte.java
@@ -4,8 +4,9 @@ public class QuestaoVinte {
 
 	public static void main(String[] args) {
 		for (int i = 0; i < 3; i++) {
-			if ( i % 2 == 0)
+			if ( i % 2 == 0) {
 				System.out.println(i);
+			}
 
 			i = i * 2;
 		}
